f6b6e3feac62cc602ee038616fc05e6fa95331a0
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-11-29 15:26:20 +0100

    feat: crearte widget

---
+++ frontend/components/widget.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image';
+
+export default function widget({children, name, image}) {
+  return (
+    <div className='containerWidget'>
+        <div className='header'>
+            <Image src={image} width={30} height={30}/>
+            <h2>{name}</h2>
+        </div>
+        {children}
+    </div>
+  )
+}
