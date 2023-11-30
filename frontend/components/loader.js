da9fe99a7cf1781e0ed65e62e283f46269e6f7c8
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-11-30 14:00:53 +0100

    feat: Create loader component

---
+++ frontend/components/loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function loader() {
+  return (
+    <div class="typing-indicator">
+        <div class="typing-circle"></div>
+        <div class="typing-circle"></div>
+        <div class="typing-circle"></div>
+        <div class="typing-shadow"></div>
+        <div class="typing-shadow"></div>
+        <div class="typing-shadow"></div>
+    </div>
+  )
+}
