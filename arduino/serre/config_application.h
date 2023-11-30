b0f986bb4f2afe58cdca4e84b855cda69836df99
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-11-30 12:31:29 +0100

    feat: arduino code sending temperature data

---
+++ arduino/serre/config_application.h
@@ -0,0 +1,25 @@
+
+#define ACTIVATION_MODE     		OTAA
+#define CLASS						        CLASS_A
+#define SPREADING_FACTOR    		7
+#define ADAPTIVE_DR         		true
+#define CONFIRMED           		false
+#define PORT                		15
+
+#define SEND_BY_PUSH_BUTTON 		false
+#define FRAME_DELAY         		10000
+//#define PAYLOAD_HELLO				    true
+//#define PAYLOAD_TEMPERATURE    	false
+//#define CAYENNE_LPP_         		false
+#define LOW_POWER           		false
+
+String devEUI = "70B3D57ED0063101";
+
+// Configuration for ABP Activation Mode
+String devAddr = "00000000";
+String nwkSKey = "00000000000000000000000000000000";
+String appSKey = "00000000000000000000000000000000";
+
+// Configuration for OTAA Activation Mode
+String appKey = "49667693E9B64312D59F8517E5806AFA"; 
+String appEUI = "0000000000000000";
